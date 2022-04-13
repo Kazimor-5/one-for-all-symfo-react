cefd1dd1d8d2c7d2f0fb91dd3c05f2b2508a1726
Alex <93370274+Kazimor-5@users.noreply.github.com>
2022-04-13 17:54:12 +0200

    adding animations

--- assets/components/landing page/Constat.js
+++ assets/components/landing page/Constat.js
@@ -15,12 +15,18 @@ const Constat = () => {
           notre constat
         </h2>
       </article>
-      <article className='constat-content'>
-        <div className='hand-img-container'>
+      <article className='constat-content' ref={containerRef}>
+        <div
+          className={
+            isVisible
+              ? 'hand-img-container slide-right active'
+              : 'hand-img-container slide-right'
+          }
+        >
           <img src='./assets/constat/imaneConstat.png' alt='' className='img' />
         </div>
-        <div className='constat-text'>
-          <p>
+        <div className='constat-text' ref={containerRef}>
+          <p className={isVisible ? 'slide-left active' : 'slide-left'}>
             <span className='purple bold'>La crise du Covid-19</span> a révélé
             les failles de notre système mondial à réagir face à une crise
             sanitaire d'ampleur internationale. Les gouvernements ont montré
@@ -28,13 +34,13 @@ const Constat = () => {
             efficaces. Chaque pays se comparant pour évaluer sa gestion de la
             crise.
           </p>
-          <p>
+          <p className={isVisible ? 'slide-right active' : 'slide-right'}>
             <span className='purple bold'>Le conflit en Ukraine</span> est un
             autre témoignage de l'incapacité des organisations mondiales à
             maintenir la paix du fait de leurs interdépendances économiques. Ce
             modèle sociètale pour l'Humanité est menacé.
           </p>
-          <p>
+          <p className={isVisible ? 'slide-right active' : 'slide-right'}>
             Il semble aujourd'hui plus que nécessaire de proposer une autre
             voie, plus respectueuse des êtres qui peuplent la Terre grâce à un{' '}
             <span className='purple bold'>Altruisme mondial</span>, sans
