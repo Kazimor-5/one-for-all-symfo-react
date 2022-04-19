cc35099a81f27a68dd4dfe9dbc5f57fcd4ad3ec1
Alex <93370274+Kazimor-5@users.noreply.github.com>
2022-04-19 11:45:19 +0200

    finishing formContatc

--- assets/App.js
+++ assets/App.js
@@ -1,6 +1,7 @@
 // !COMPONENTS
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
+import ContactForm from './components/formulaire contact/ContactForm';
 // ! FILES
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route index element={<LandingPage />} />
+        <Route path='/contact' element={<ContactForm />} />
       </Routes>
     </Router>
   );
---
+++ assets/components/formulaire contact/ContactForm.js
@@ -0,0 +1,143 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLasteName] = useState('');
+  const [email, setEmail] = useState('');
+  const [motif, setMotif] = useState('');
+  const [object, setObject] = useState('');
+  const [text, setText] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (firstName && lastName && email && motif && object && text) {
+      const message = {
+        id: new Date().getTime().toString(),
+        firstName,
+        lastName,
+        email,
+        motif,
+        object,
+        text,
+      };
+      setMessages((item) => {
+        return [...item, message];
+      });
+      setFirstName('');
+      setLasteName('');
+      setEmail('');
+      setMotif('');
+      setObject('');
+      setText('');
+    }
+  };
+
+  return (
+    <section className='section-contact-form'>
+      <form onSubmit={handleSubmit} className='form'>
+        <h2 className='title'>nous contacter</h2>
+        <div className='form-row'>
+          <label htmlFor='firstName' className='form-label'>
+            Prénom:
+          </label>
+          <input
+            required
+            type='text'
+            id='firstname'
+            className='form-input'
+            placeholder='Prénom'
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        <div className='form-row'>
+          <label htmlFor='lastName' className='form-label'>
+            Nom:
+          </label>
+          <input
+            required
+            type='text'
+            className='form-input'
+            id='lastName'
+            placeholder='Nom'
+            value={lastName}
+            onChange={(e) => setLasteName(e.target.value)}
+          />
+        </div>
+        <div className='form-row'>
+          <label htmlFor='email' className='form-label'>
+            Email:
+          </label>
+          <input
+            required
+            type='email'
+            id='email'
+            className='form-input'
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className='form-row'>
+          <label htmlFor='motif' className='form-label'>
+            Motif
+          </label>
+          <select
+            required
+            name='motif'
+            id='motif'
+            className='motif'
+            value={motif}
+            onChange={(e) => setMotif(e.target.value)}
+          >
+            <option value=''>Choisir un motif de message</option>
+            <option value='relation de presse'>Relation de presse</option>
+            <option value='recrutement'>Recrutement</option>
+          </select>
+        </div>
+        <div className='form-row'>
+          <label htmlFor='objet' className='form-label'></label>
+          <input
+            type='text'
+            id='objet'
+            placeholder='Objet du message'
+            value={object}
+            onChange={(e) => setObject(e.target.value)}
+            className='form-input'
+          />
+        </div>
+        <div className='form-row'>
+          <textarea
+            name=''
+            id=''
+            className='form-textarea'
+            placeholder='Entrez votre message ...'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
+        </div>
+        <button type='submit' className='btn btn-block'>
+          Envoyer le message
+        </button>
+      </form>
+      {messages.map((message) => {
+        const { id, firstName, lastName, email, motif, object, text } = message;
+
+        return (
+          <div key={id} className='item'>
+            <h4>Prénom: {firstName}</h4>
+            <h4>Nom: {lastName}</h4>
+            <p>Email: {email}</p>
+            <p>Motif: {motif}</p>
+            <p>Objet: {object}</p>
+            <p>Message: {text}</p>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default ContactForm;
