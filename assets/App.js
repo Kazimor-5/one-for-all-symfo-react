5472538bc0c5ee3ec0045cb8a9041913945c4128
Alex <93370274+Kazimor-5@users.noreply.github.com>
2022-04-19 11:48:15 +0200

    adding addEvent route and addEvent.js

--- assets/App.js
+++ assets/App.js
@@ -1,6 +1,7 @@
 // !COMPONENTS
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
+import AddEvents from './components/addEvent/AddEvents';
 // ! FILES
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route index element={<LandingPage />} />
+        <Route path='/addEvent' element={<AddEvents />} />
       </Routes>
     </Router>
   );
