a9fb8d816b4db636500ae5a3a84882dfe3ba5cd8
Alex <93370274+Kazimor-5@users.noreply.github.com>
2022-04-20 15:20:59 +0200

    adding addEvetn form

--- assets/components/addEvent/AddEvents.js
+++ assets/components/addEvent/AddEvents.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const url = 'http://localhost:8000/api/addEvent';
 
 const AddEvents = () => {
-  return <div>AddEvents</div>;
+  const [title, setTitle] = useState('');
+  const [file, setFile] = useState();
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('fileName', file.name);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+    axios
+      .post(url, formData, config)
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  return (
+    <section className='section-addEvent'>
+      <form onSubmit={handleSubmit} className='form'>
+        <h2 className='title'>ajouter un évènement</h2>
+        <article className='form-row'>
+          <label htmlFor='title' className='form-label'>
+            Titre:
+          </label>
+          <input
+            type='text'
+            id='title'
+            className='form-input'
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </article>
+        <article className='form-row'>
+          <label htmlFor='file' className='form-label'>
+            Télecharger un fichier:
+          </label>
+          <input
+            type='file'
+            name='file'
+            id='file'
+            className='form-input'
+            onChange={handleChange}
+          />
+        </article>
+        <article className='form-row'>
+          <label htmlFor='description' className='form-label'>
+            Description de l'évènement:
+          </label>
+          <textarea
+            name='description'
+            id='description'
+            className='form-textarea'
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+        </article>
+        <button className='btn btn-block'>ajouter l'évènement</button>
+      </form>
+    </section>
+  );
 };
 
 export default AddEvents;
