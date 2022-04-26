77b6d1994c5cb8ad0115b0a5880511745a12ada7
kahina-91 <kahinabenaouadi91@gmail.com>
2022-04-26 13:37:55 +0200

    page_nos_activités_commerciales-animation

--- assets/components/nos activités commerciales/AcivitesCommerciales.js
+++ assets/components/nos activités commerciales/AcivitesCommerciales.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const ActivitesCommerciales = () => {
     const myStyle={
@@ -8,6 +10,60 @@ const ActivitesCommerciales = () => {
                 backgroundRepeat: 'no-repeat',
                
             };
+    
+    const slideInLeft = (elem, delay, duration) => {
+        gsap.fromTo(
+            elem,
+            {
+                opacity: 0,
+                x: -50,
+            },
+            {
+                opacity: 1,
+                x: 0,
+                duration: duration || 0.3,
+                delay: delay || 0.4,
+                scrollTrigger: {
+                    trigger: elem,
+                    start: 'top bottom',
+                    end: 'bottom center',
+                },
+            }
+        );
+    };
+
+    const slideInBottom = (elem, delay, duration) => {
+        gsap.fromTo(
+            elem,
+            {
+                opacity: 0,
+                y: 50,
+                },
+                {
+                opacity: 1,
+                y: 0,
+                duration: duration || 0.3,
+                delay: delay || 0.4,
+                scrollTrigger: {
+                    trigger: elem,
+                    start: 'top bottom',
+                    end: 'bottom center',
+                },
+            }
+        );
+    };
+
+    useEffect(() => {
+        slideInBottom('.image-man-women');
+      }, []);
+
+      useEffect(() => {
+        slideInBottom('.lien-one4all-california');
+      }, []);
+    
+      useEffect(() => {
+        slideInLeft('.image-titre-activite');
+      }, []);
 
     return <>
         <section className="section-nos-activites-comm" style={myStyle}>
@@ -22,21 +78,21 @@ const ActivitesCommerciales = () => {
                     <h2>UN NOUVEAU LIFESTYLE</h2>
                     <h3 className="capital-activites">épicurien, fashion et altruiste...</h3>
                     <h3>Le mantra de la marque au 3ème oeil.</h3><br/>
-                    <h3>Apprètez-vous à changer de dimension !!!</h3>
+                    <h3 style={{marginTop: -10}}>Apprètez-vous à changer de dimension !!!</h3>
                 </div>
             </article>
             <article className="wrap">
                 
                 <article className="activites-content2">
-                    <div className="">
+                    <div className="image-man-women">
                         <img src="./assets/activite_commerciales/Plcqaltruiste-42x15-02.png" />
                     </div>
                     <div className="paragraph-activite2">
                         <p>
-                        Choisir #ONE-IV-ALL California c’est faire le choix de vous faire plaisir en soutenant une cause.
+                        Choisir #ONE-<span className="store-font-yellow">IV</span>-ALL California c’est faire le choix de vous faire plaisir en soutenant une cause.
                         </p>
-                        <p>Grâce à #ONE-IV-ALL California, la consommation n’est plus une finalité mais un moyen de financer des actions altruistes.</p>
-                        <button className="btn"><a href="https://www.one4all-california.world/"> www.one4all-california.world/</a></button>
+                        <p>Grâce à #ONE-<span className="store-font-yellow">IV</span>-ALL California, la consommation n’est plus une finalité mais un moyen de financer des actions altruistes.</p>
+                        <button className="btn lien-one4all-california"><a href="https://www.one4all-california.world/"> www.one4all-california.world/</a></button>
                     </div>
                 </article>
             </article>
