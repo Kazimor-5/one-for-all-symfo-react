f71b729a64a91c2d0514b7a552194b17053525e3
kahina-91 <kahinabenaouadi91@gmail.com>
2022-04-26 11:20:11 +0200

    page_nos_activités_commerciales

--- assets/components/nos activités commerciales/AcivitesCommerciales.js
+++ assets/components/nos activités commerciales/AcivitesCommerciales.js
@@ -13,24 +13,25 @@ const ActivitesCommerciales = () => {
         <section className="section-nos-activites-comm" style={myStyle}>
             <article className="image-titre-activite">
                 <img src="./assets/activite_commerciales/Titre-Nos-activités.png" />
-            </article><article className="activites-content1">
-                    <div className="image-logo-activites">
-                        <img src="./assets/activite_commerciales/One4All-california-logo-blanc.png" />
-                    </div>
-                    <div className="paragraph-activite1">
-                        <h2>UN NOUVEAU LIFESTYLE</h2>
-                        <h3 className="capital-activites">épicurien, fashion et altruiste...</h3>
-                        <h3>Le mantra de la marque au 3ème oeil.</h3>
-                        <h3>Apprètez-vous à changer de dimension !!!</h3>
-                    </div>
-                </article>
+            </article>
+            <article className="activites-content1">
+                <div className="image-logo-activites">
+                    <img src="./assets/activite_commerciales/One4All-california-logo-blanc.png" />
+                </div>
+                <div className="paragraph-activite1">
+                    <h2>UN NOUVEAU LIFESTYLE</h2>
+                    <h3 className="capital-activites">épicurien, fashion et altruiste...</h3>
+                    <h3>Le mantra de la marque au 3ème oeil.</h3><br/>
+                    <h3>Apprètez-vous à changer de dimension !!!</h3>
+                </div>
+            </article>
             <article className="wrap">
                 
                 <article className="activites-content2">
                     <div className="">
                         <img src="./assets/activite_commerciales/Plcqaltruiste-42x15-02.png" />
                     </div>
-                    <div className="">
+                    <div className="paragraph-activite2">
                         <p>
                         Choisir #ONE-IV-ALL California c’est faire le choix de vous faire plaisir en soutenant une cause.
                         </p>
