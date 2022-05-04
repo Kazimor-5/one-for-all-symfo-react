24e4a039446479ef323868f9db898a317c84b8b4
VirginieT <vtouzalin33@gmail.com>
2022-05-04 15:26:51 +0200

    serializer + fixtures added = apiplatform test in DB ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Mission;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,18 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 10; $i++) {
+            $mission = new Mission();
+            $mission->setCategory('category');
+            $mission->setName('name');
+            $mission->setScope('scope');
+            $mission->setDescription('description');
+            $mission->setPicture('picture');
+            $mission->setDateStart(new \DateTime());
+            $mission->setDateEnd(new \DateTime());
+            $mission->setStatus('status');
+            $manager->persist($mission);
+            }
 
         $manager->flush();
     }
