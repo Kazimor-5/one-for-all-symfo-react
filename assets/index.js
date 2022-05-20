9fcd9e82e753c99941f1ffda7ceab0087ef68546
Alex <93370274+Kazimor-5@users.noreply.github.com>
2022-05-20 17:07:33 +0200

    adding context

--- assets/index.js
+++ assets/index.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import AppProvider from './context';
 import './styles/normalize.css';
 import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
